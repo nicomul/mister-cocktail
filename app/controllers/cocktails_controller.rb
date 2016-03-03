c25ddf59c2f09a45dc2a3ba19d7b12a342e6c652
Nicolas Mulliez <mulliez.nicolas@gmail.com>
2016-03-03 14:20:06 +0100

    cocktail_controller

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,45 @@
+class CocktailsController < ApplicationController
+  before_action :set_restaurant, only: [ :show, :update, :edit, :destroy ]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to action: 'index'
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
